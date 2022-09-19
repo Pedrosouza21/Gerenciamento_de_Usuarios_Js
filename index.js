f2808f52950eb3a10d05b6d40f456103e3fbdaee
Pedrosouza21 <pedrohenriqueoliveiradesouza9@gmail.com>
2022-09-19 11:55:46 -0300

    Manipulating the DOM

---
+++ index.js
@@ -0,0 +1,13 @@
+// Quando  tem um () então é um Metodo (ação)
+
+var nome = document.querySelector("#exampleInputName");
+
+// serve para colocar no consoler do developer tools
+// console.log(nome);
+
+nome.value = "Pedro";
+nome.style.color = "blue";
+
+
+
+
