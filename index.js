6cbfd2cd21a76f5e47f063a73fab234244a5c741
Pedrosouza21 <pedrohenriqueoliveiradesouza9@gmail.com>
2022-09-19 12:20:48 -0300

    add variables and selectors

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 // Quando  tem um () então é um Metodo (ação)
+// var = variavel
 
 var nome = document.querySelector("#exampleInputName");
+var gender = document.querySelectorAll("#form-use-create[name=gender]:checked");
+var birth = document.querySelector("exampleInputBirth");
+var country = document.querySelector("#exampleInputCountry");
+var email = document.querySelector("#exampleInputEmail");
+var password = document.querySelector("#exampleInputPassword");
+var photo = document.querySelector("#exampleInputFile");
+var admin = document.querySelector("exampleInputAdmin");
 
 // serve para colocar no consoler do developer tools
 // console.log(nome);
 
 nome.value = "Pedro";
 nome.style.color = "blue";
+
+
 
 
 
