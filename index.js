b4e42ee8090cb915f03ed7470575880239b07832
Pedrosouza21 <pedrohenriqueoliveiradesouza9@gmail.com>
2022-09-19 13:00:07 -0300

    incrementing forEach

--- index.js
+++ index.js
@@ -12,9 +12,20 @@ var admin = document.querySelector("exampleInputAdmin");
 
 // serve para colocar no consoler do developer tools
 // console.log(nome);
+//forEach é um laço que percorre Array para cada item , execute uma função
 
-nome.value = "Pedro";
-nome.style.color = "blue";
+
+// function = um trecho de codigo que executa alguma coisa e que retorna algo, para cada campo de um formulário, ele irá executa mais de uma vez 
+//function  anonima = função sem nome
+
+var fields = document.querySelectorAll("#form-user-create [name]");
+
+fields.forEach(function(field, index){
+    console.log(field.id, field.name, field.value, field.checked,index);
+
+
+});
+
 
 
 
