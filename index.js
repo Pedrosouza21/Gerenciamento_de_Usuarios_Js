64aa38b175384b3cc5f4305991c9b08e621e6a9a
Pedrosouza21 <pedrohenriqueoliveiradesouza9@gmail.com>
2022-09-20 09:02:02 -0300

    Working with events

--- index.js
+++ index.js
@@ -10,25 +10,46 @@
 // function = um trecho de codigo que executa alguma coisa e que retorna algo, para cada campo de um formulário, ele irá executa mais de uma vez 
 //function  anonima = função sem nome
 
+// add Eventlist executa uma função, um evento
+// document.querySelectorAll("button").forEach(function () {
+
+//     this.addEventListener("click", function () {
+//         console.log("clicou!");
+//     });
+// });
+
+
+//Usando eventos
 var fields = document.querySelectorAll("#form-user-create [name]");
 var user = {};
 
-fields.forEach(function (field, index) {
-    console.log(field.id, field.name, field.value, field.checked, index);
+document.getElementById("form-user-create").addEventListener("submit", function (event) {
 
-    // == é comparação, === compara valor e tipo de dado
-    if (field.name == "gender") {
-        if (field.checked) {
-            user[field.name] = field.value;
+    event.preventDefault();
+
+    fields.forEach(function (field, index) {
+        console.log(field.id, field.name, field.value, field.checked, index);
+
+        // == é comparação, === compara valor e tipo de dado
+        if (field.name == "gender") {
+            if (field.checked) {
+                user[field.name] = field.value;
+            }
+
+
+        } else {
+            user[field.name] = field.value
         }
 
-
-    } else {
-        user[field.name] = field.value
-    }
+    });
+    console.log(user);
 
 });
-console.log(user);
+
+
+
+
+
 
 
 
