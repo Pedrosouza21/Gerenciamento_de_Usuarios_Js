590da7d8cc11515cc9f0d1a6e55426d8d0358648
Pedrosouza21 <pedrohenriqueoliveiradesouza9@gmail.com>
2022-09-20 09:59:29 -0300

    implementing functions

--- index.js
+++ index.js
@@ -23,12 +23,38 @@
 var fields = document.querySelectorAll("#form-user-create [name]");
 var user = {};
 
+
+// Html e Jascript trabalhando junto com o formulario
+function addLine(dataUser){
+
+    var tr = document.createElement("tr");
+  
+
+    tr.innerHTML = ` 
+        <tr>
+            <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
+            <td>${dataUser.name}</td>
+            <td>${dataUser.email}</td>
+            <td>${dataUser.admin}</td>
+            <td>${dataUser.birth}</td>
+
+            <td>
+                <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+                <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+            </td>
+        </tr> 
+    ` ;
+
+    document.getElementById("table-users").appendChild(tr);
+}
+
+
+
 document.getElementById("form-user-create").addEventListener("submit", function (event) {
 
     event.preventDefault();
 
     fields.forEach(function (field, index) {
-        console.log(field.id, field.name, field.value, field.checked, index);
 
         // == é comparação, === compara valor e tipo de dado
         if (field.name == "gender") {
@@ -42,7 +68,8 @@ document.getElementById("form-user-create").addEventListener("submit", function 
         }
 
     });
-    console.log(user);
+
+    addLine(user);
 
 });
 
